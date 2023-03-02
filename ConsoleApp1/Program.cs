454ccdd520422dc8baf896d6c5fd66b3e76784c4
Игорь Павленко <illusiopard@gmail.com>
2023-03-02 20:57:58 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IAds ads = new ShowAdvertisement();
+            List<User> usersList = new List<User>();
+            usersList.Add(new User() { Login = "John_Wick", Name = "Вася", IsPremium = false });
+            usersList.Add(new User() { Login = "SoulDestroyer", Name = "Константин", IsPremium = true });
+            usersList.Add(new User() { Login = "PavelSmolyaninov", Name = "Павел", IsPremium = true });
+            usersList.Add(new User() { Login = "Sergey78", Name = "Сергей", IsPremium = false });
+            usersList.Add(new User() { Login = "Venya", Name = "Вениамин", IsPremium = true });
+
+            foreach (User user in usersList)
+            {
+                if (user.IsPremium == false)
+                {
+                    Console.WriteLine($"Ну, привет, {user.Name},  {user.Login}. Не желаешь немного рекламы:");
+                    ads.ShowAds();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"\tДобро пожаловать {user.Name}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
